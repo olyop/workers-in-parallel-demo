828e91aced14c36036dd9a43920f618df8babf9b
Oliver Plummer <oliver.plummer@outlook.com>
2024-03-30 16:58:43 +1100

    Project template

---
+++ src/index.tsx
@@ -0,0 +1,13 @@
+import { createElement } from "react";
+import { createRoot } from "react-dom/client";
+import "tailwindcss/tailwind.css";
+
+const container = document.getElementById("root");
+
+if (!container) {
+	throw new Error("No root element found");
+}
+
+const root = createRoot(container);
+
+root.render(<p>Hello</p>);
